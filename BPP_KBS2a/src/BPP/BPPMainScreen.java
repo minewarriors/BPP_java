245f1372d44b36454cbb695a2b3bac9afb2b5b59
BartEekelder <bart_eekelder@outlook.com>
2018-05-08 13:31:39 +0200

    Hoofdscherm

--- BPP_KBS2a/src/BPP/BPPMainScreen.java
+++ BPP_KBS2a/src/BPP/BPPMainScreen.java
@@ -1,5 +1,82 @@
 package BPP;
 
-public class BPPMainScreen {
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class BPPMainScreen extends JFrame implements ActionListener {
+
+    private Product product;
+
+    private String[] Algorithms = {"Best-fit Decreasing", "Bin Completion", "First Fit", "Own Method"};
+
+    private JLabel jlXML;
+    private JButton jbUploadXML;
+    private JLabel jlNumberTimes;
+    private JTextField jtfNumber;
+    private JButton jbStart;
+    private JLabel jlAlgorithm;
+    private JButton jbStop;
+    private JLabel jlAddProduct;
+    private JTextField jtfSize;
+    private JButton jbAddProduct;
+    private JButton jbAddRandom;
+
+    public BPPMainScreen(Product product) {
+        this.product = product;
+
+        setTitle("BPP");
+        setSize(1000, 800);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jlXML = new JLabel("Upload XML file:");
+        add(jlXML);
+        jbUploadXML = new JButton("Choose file");
+        jbUploadXML.addActionListener(this);
+        add(jbUploadXML);
+
+        jlNumberTimes = new JLabel("Number of times:");
+        add(jlNumberTimes);
+        jtfNumber = new JTextField(2);
+        add(jtfNumber);
+
+        jbStart = new JButton("Start");
+        jbStart.addActionListener(this);
+        add(jbStart);
+
+        jlAlgorithm = new JLabel("BPP Algorithm:");
+        add(jlAlgorithm);
+        JComboBox AlgorithmList = new JComboBox(Algorithms);
+        AlgorithmList.setSelectedIndex(3);
+        AlgorithmList.addActionListener(this);
+        add(AlgorithmList);
+
+        jbStop = new JButton("Stop");
+        jbStop.addActionListener(this);
+        add(jbStop);
+
+        jlAddProduct = new JLabel("Add product:");
+        add(jlAddProduct);
+        jtfSize = new JTextField(1);
+        add(jtfSize);
+        jbAddProduct = new JButton("Add");
+        jbAddProduct.addActionListener(this);
+        add(jbAddProduct);
+        jbAddRandom = new JButton("Add random");
+        jbAddRandom.addActionListener(this);
+        add(jbAddRandom);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 
 }
--- BPP_KBS2a/src/BPP/Main.java
+++ BPP_KBS2a/src/BPP/Main.java
@@ -1,11 +1,12 @@
 package BPP;
 
 public class Main {
-        public static void main(String[] args) {
 
-           Product A1 = new Product(1,2,3,"green","M");
-           System.out.println(A1);
-           
+    public static void main(String[] args) {
+
+        Product A1 = new Product(1, 2, 3, "green", "M");
+        System.out.println(A1);
+        BPPMainScreen headscreen = new BPPMainScreen(A1);
     }
-        
+
 }
