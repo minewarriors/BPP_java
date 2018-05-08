66ebd2e4faacfb08a3b9d395eb385f0adfab2a5c
Christiaan van den Berg <chrsitiaan8642@gmail.com>
2018-05-08 13:35:42 +0200

    sefesf

--- BPP_KBS2a/src/BPP/Main.java
+++ BPP_KBS2a/src/BPP/Main.java
@@ -1,25 +1,23 @@
 package BPP;
 
+import static java.awt.PageAttributes.MediaType.A1;
+
 public class Main {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
 
-        Product A1 = new Product(1, 2, 3, "green", "M");
-        System.out.println(A1);
-        BPPMainScreen headscreen = new BPPMainScreen(A1);
-=======
            Product A1 = new Product(1,2,3,"green",10);
            Product A2 = new Product("green",10);
           // System.out.println(A1);
           // System.out.println(A2);
+          
+           BPPMainScreen headscreen = new BPPMainScreen(A1);
            
            RandomProductArray b2 = new RandomProductArray();
            b2.print();
            System.out.println(b2.getSize());
            System.out.println(b2.getProductSize(1));
            System.out.println(b2.getProductColor(1));
->>>>>>> e490a7e7b235527683445708df58d1253ba6845c
     }
 
 }
