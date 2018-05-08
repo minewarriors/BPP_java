16a97ce109c74625ef54975d191e5d735d92a036
Christiaan van den Berg <chrsitiaan8642@gmail.com>
2018-05-08 11:58:49 +0200

    edit

--- BPP_KBS2a/src/BBP/Main.java
+++ BPP_KBS2a/src/BBP/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BBP;
 
 /**
@@ -10,5 +5,11 @@ package BBP;
  * @author Christiaan
  */
 public class Main {
-    
+        public static void main(String[] args) {
+
+           Product A1 = new Product(1,2,3,"green","M");
+           System.out.println(A1);
+           
+    }
+        
 }
--- BPP_KBS2a/src/BBP/Product.java
+++ BPP_KBS2a/src/BBP/Product.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BBP;
 
 /**
@@ -10,5 +5,45 @@ package BBP;
  * @author Christiaan
  */
 public class Product {
+
+    private int productId;
+    private int x;
+    private int y;
+    private String color;
+    private String size;
+
+    public Product(int productId, int x, int y, String color, String size) {
+        this.productId = productId;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.size = size;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "productId=" + productId + ", x=" + x + ", y=" + y + ", color=" + color + ", size=" + size + '}';
+    }
+    
     
 }
